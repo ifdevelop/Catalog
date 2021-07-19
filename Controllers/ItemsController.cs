a1af087cfb4b826e512324a75bac9c93a8d0b3b9
ifdevelop <if.dev402@gmail.com>
2021-07-19 23:30:29 +0300

    Initialization of Project, InMem Repository and Controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if(item is null)
+                return NotFound();
+
+            return item;
+        }
+    }
+}
